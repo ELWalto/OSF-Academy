9710d04e64f1e48a7d428a07a3bf90b2f7546394
Walter Carvalho <walterclean@gmail.com>
2022-01-05 12:09:27 -0300

    feat{AppRoutes}: Routes and style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route ,Routes  } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+      <Route path="/" exact component={Home}/>
+      </Routes>
+
+    </Router>
   );
 }
 
