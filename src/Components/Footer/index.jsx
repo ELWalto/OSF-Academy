92292dbf322a47120197b88925ef7206124da57e
Walter Carvalho <walterclean@gmail.com>
2022-01-06 15:13:07 -0300

    feat{Footer}: simple footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FooterStyle } from './style'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+
+            <FooterStyle>
+                
+                &copy; {new Date().getFullYear()}. All rights reserved      
+            </FooterStyle>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
+    
---
+++ src/Components/Footer/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const FooterStyle = styled.footer`
+  bottom: 0;
+  width: 100%;
+  max-width: 1400px;
+  text-align: center;
+  align-items: center;
+  align-content: flex-start;
+  text-align: center;
+  margin-bottom: 10px;
+  line-height: 50px;
+`;
