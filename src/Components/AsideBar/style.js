e5af8aa61dcac4e736877a56b7acdb83d4095e09
Walter Carvalho <walterclean@gmail.com>
2022-01-10 11:50:23 -0300

    feat{AsideBar}: Styles

---
+++ src/Components/AsideBar/style.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const AsideBarStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-wrap: nowrap;
+  flex:2;
+  text-align:justify;
+  align-self: flex-start;
+  margin: .5rem;
+  padding: 1.3rem;
+  line-height: 50px;
+  font-weight: 500;
+    
+`;
+
+export const AsideSectionStyle = styled.section`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    width: 100%;
+    position: relative;
+    margin-right: 20px;
+    margin-left:0;
+    
+
+`;
+
+export const UlStyle= styled.ul`
+    text-decoration: none;
+    justify-content: center;
+    text-align: center;
+    margin: 0 auto;
+    
+`
+
+export const LiStyle= styled.li`
+    text-decoration: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    
+`
