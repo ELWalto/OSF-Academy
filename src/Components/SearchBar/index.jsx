dea6d3d31ae5480e82982ad2e36a09e25f81f0d9
Walter Carvalho <walterclean@gmail.com>
2022-01-17 18:50:07 -0300

    Navmenu

---
+++ src/Components/SearchBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavMenu } from '../Navbar/style'
+
+const SearchBar = () => {
+  return (
+    <NavMenu>
+       <input type="text" placeholder="Busque seu produto"/>
+    </NavMenu>
+  )
+}
+
+export default SearchBar
+
